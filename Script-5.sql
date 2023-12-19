488b9319045c1365fe9d65cfd350dbdc566b9c46
Alexandr Kovalev <alex_kov@inbox.ru>
2023-12-19 23:14:52 +0500

    HW reworked

--- Script-5.sql
+++ Script-5.sql
@@ -17,9 +17,16 @@ where year >= 2018 and year <= 2020;
 select nickname from artist a 
 where nickname not like '% %' ;
 
--- п.5 Небольшие изменения в коде: my --> me 
+-- п.5 
 select name from track
-where name like '% me %' or name like '% мой %';
+	where name ilike '% my %'
+	or name ilike 'my %'
+	or name ilike '% my'
+	or name ilike 'my'
+	or name ilike 'мой %'
+	or name ilike '% мой'
+	or name ilike 'мой'
+	or name ilike '% мой %';
 
 
 
@@ -30,9 +37,9 @@ join style_artist sa on s.id  = sa.style_id
 group by name;
 
 -- п.2
-select year, count(t.id) from track t 
+select count(t.id) from track t 
 join album a on t.album_id = a.id
-where year >= 2018 and year <= 2020
+where year between 2018 and 2020
 group by year;
 
 -- п.3 
@@ -40,15 +47,21 @@ select a.name, avg(t.duration) from album a
 join track t on a.id = t.album_id 
 group by a.name;
 
--- п.4
-select name from artist a 
-join album a2 on a.id = a2.id 
-where year != 2020
+-- п.4 Updated
+select nickname from artist a 
+where a.nickname not in (
+	select a.nickname from artist a
+	join artist_album aa on a.id = aa.artist_id
+	join album a2 on a2.id = aa.artist_id
+	where a2.year = 2020)
 
--- п.5
-select name from mix m 
-join artist a on m.id = a.id 
-where a.nickname = 'Vanessa Mae';
-
+-- п.5 Updated
+select distinct m.name from mix m 
+join track_mix tm  on m.id = tm.mix_id 
+join track t on t.id = tm.track_id 
+join album a on a.id = t.album_id 
+join artist_album aa on a.id = aa.album_id 
+join artist a2 on aa.artist_id = a2.id 
+where a2.nickname = 'Vanessa Mae'
 
 
